fb26bd9071c7ea61340b13a31717fa348f40708d
Jay Gupta <jaygupt1973@gmail.com>
2021-09-18 09:56:03 -0700

    Finished Gatsby Tutorial Part 3

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,16 +1,16 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const AboutPage = () => {
   return (
-    <main>
-      <Layout pageTitle="About Me">
-        <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
-        <p>My favorite food is mac-and-cheese.</p>
-        <p>My favorite vacation spot is Disneyland in Los Angeles, CA.</p>
-      </Layout>
-    </main>
+    <Layout pageTitle="About Me">
+      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+      <StaticImage 
+        alt="Health Ledger as The Joker" 
+        src="../images/thejoker.jpeg"
+      />
+    </Layout>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,16 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
-    <main>
       <Layout pageTitle="Home Page">
         <p>I'm making this by following the Gatsby Tutorial.</p>
+        <StaticImage 
+          alt="Christian Bale as The Dark Knight" 
+          src="https://static.wikia.nocookie.net/batman/images/8/8f/Christian_Bale_as_The_Dark_Knight.jpg/revision/latest?cb=20210312234853"
+        />
       </Layout>
-    </main>
   )
 }
 
